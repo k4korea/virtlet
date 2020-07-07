91cfed9e52fd0e6de27386dc629badd7e9fc80d8
k4korea <love7310@naver.com>
2020-07-07 14:01:08 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:7-onbuild
+
+# set a health check
+HEALTHCHECK --interval=5s \
+            --timeout=5s \
+CMD curl -f http://127.0.0.1:80 || exit 1
+
+# tell docker what port to expose
+EXPOSE 80
---
+++ index.js
@@ -0,0 +1,31 @@
+var express =require('express');
+var fs = require('fs');
+var app = express();
+
+app.get('/index',function(req,res){
+    fs.readFile('index.html', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'} ) ;
+      res.end(data);
+    });
+    
+});
+
+app.get('/',function(req,res){
+    fs.readFile('main.html', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'})  ;
+      res.end(data);
+    });
+    
+});
+
+app.get('/imgs',function(req,res){
+    fs.readFile('VIRTLET.jpg', function(error, data){
+      res.writeHead(200, {'Content-Type': 'text/html'})  ;
+      res.end(data);
+    });
+    
+});
+
+app.listen( 80, function(){
+    console.log('Server Start');
+})
